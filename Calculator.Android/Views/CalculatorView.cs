3bf8145ce2e809b25bc6bdd0c36035ffcc191a0b
Andres <Andres@DESKTOP-M9II48O>
2019-11-20 16:06:56 -0500

    Finish Android

---
+++ Calculator.Android/Views/CalculatorView.cs
@@ -0,0 +1,18 @@
+﻿
+using Android.App;
+using Android.OS;
+using Calculator.Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Calculator.Android.Views
+{
+    [Activity(Label = "@string/app_name")]
+    public class CalculatorView : MvxActivity<CalViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.CalculatorPage);
+        }
+    }
+}
